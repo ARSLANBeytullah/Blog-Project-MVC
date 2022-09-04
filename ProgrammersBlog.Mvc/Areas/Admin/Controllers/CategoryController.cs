844a4fd6c31db0e9caeaeccfc690f5b061529ff8
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-09-04 14:46:10 +0300

    ControllerExtensions Added

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Mvc.Areas.Admin.Models;
 using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Extensions;
 using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 using System;
 using System.Collections.Generic;
@@ -24,9 +27,20 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
             return View(result.Data);
         }
 
+        [HttpGet]
         public IActionResult Add()
         {
             return PartialView("_CategoryAddPartial");
+        }
+
+
+        [HttpPost]
+        public async IActionResult Add(CategoryAddDto categoryAddDto)
+        {
+            var categoryAjaxModel = new CategoryAddAjaxViewModel
+            {
+                CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto), //partial view'i bir string' e çevirmemizi sağlıyor.
+            };
         }
     }
 }
---
+++ ProgrammersBlog.Mvc/Areas/Admin/Models/CategoryAddAjaxViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ProgrammersBlog.Entities.Dtos;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models
+{
+    public class CategoryAddAjaxViewModel //Mvc katmanımızın kullanacağı bir model tanımlaması yaptım. 
+    {
+        public CategoryAddDto CategoryAddDto { get; set; }
+        public string CategoryAddPartial { get; set; }
+        public CategoryDto CategoryDto { get; set; }
+    }
+}
